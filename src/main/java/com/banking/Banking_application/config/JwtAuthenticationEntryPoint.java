f0c5cc393f287bf25d71d35959c5ac91fec73f02
Sethu Madhav <sethumadhav2467@gmail.com>
2024-07-04 23:22:23 +0530

    implementing Jwt token

---
+++ src/main/java/com/banking/Banking_application/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.banking.Banking_application.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
